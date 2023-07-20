700002b71f5f6dc072156c9465ad3ffe7f38b480
betteenadenny <116335920+betteenadenny@users.noreply.github.com>
2023-07-21 00:08:27 +0530

    exercise-5.sql

--- exercise-5.sql
+++ exercise-5.sql
@@ -2,11 +2,15 @@ create database trig;
 use trig;
 create table product (pdtid int primary key,pname varchar(30),price int,qtyinstock int);
 create table sale (saleid int primary key,deliveryaddress varchar(40));
-create table saleitem(saleid int,pdtid int,qty int,primary key(saleid,pdtid));
-alter table saleitem add constraint foreign key (pdtid) references product(pdtid);
-alter table saleitem add constraint foreign key (saleid) references sale(saleid);
+create table saleitem(saleid int,pdtid int,qty int,primary key(saleid,pdtid),
+foreign key (pdtid) references product(pdtid),
+foreign key (saleid) references sale(saleid));
+
+create trigger updateAvaliableQuantityOne after insert on saleitem for each row 
+UPDATE product,saleitem SET product.qtyinstock = product.qtyinstock - saleitem.qty 
+WHERE saleitem.pdtid = product.pdtid;
+
 insert into product values(101,'soap',30,25),(102,'chocolate',100,100),(103,'pen',10,10000),(104,'pencil',5,2000),(105,'notebook',28,1000);
 insert into sale values (1,'xyz'),(2,'abc'),(3,'def'),(4,'mno'),(5,'pqr');
-create trigger updateAvaliableQuantityOne after insert on saleitem for each row update product set product.qtyinstock=product.qtyinstock-new.qtyinstock where product.pdtid = new.pdtid;
-select * from product;
-insert into saleitem values (1,101,5);
+insert into saleitem values (1,101,5),(2,102,50);
+
